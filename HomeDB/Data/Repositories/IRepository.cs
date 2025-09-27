dd3268d7a037e6cb0f570f0ccb4d44f227e7c7cc
Anton Knyazev <anton.kn59@gmail.com>
2025-09-27 15:08:43 +0500

    добавлен интерфейс IRepository

---
+++ HomeDB/Data/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeDB.Data.Repositories
+{
+    internal interface IRepository
+    {
+    }
+}
